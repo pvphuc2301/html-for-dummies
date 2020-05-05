6c8db3ec8fef62a833a878e5236e7378de429840
Phan Phuc <pvphuc2301@gmail.com>
2020-05-06 00:12:06 +0700

    practice

---
+++ network/1/index.js
@@ -0,0 +1,7 @@
+/**
+ * Ghi lại địa chỉ public IP của bạn (địa chỉ mà ai cũng có thể truy cập)
+ * Ghi lại địa chỉ private IP của bạn (địa chỉ mà những người trong cùng mạng với bạn ở nơi bạn đang ở có thể thấy và truy cập)
+ * Gợi ý: Xem video để biết cách tìm
+ * my public IP Address is 125.214.52.129
+ * my private IP Address is 192.168.1.18
+ */
