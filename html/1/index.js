407a284d452739a020a5f03b372706dc2d62c0b0
Phan Phuc <pvphuc2301@gmail.com>
2020-05-06 23:23:56 +0700

    practice

---
+++ html/1/index.js
@@ -0,0 +1,11 @@
+/**
+ * Ghi lại quá trình học HTML của bạn tại đây theo format sau:
+ * Thời gian - HTML elements
+ * Ví dụ:
+ * 2018-09-10 a, img, p, h1, h2, h3
+ * 2018-09-11 div, span, ...
+ */
+
+ /**
+  * 2020-05-6 h1, h2, h3, h4, h5, h6, hr, html, head, title, meta, body, p, br, pre, b, strong, i, em, mark, small, del, ins, sup, sub, q, blockquote, abbr, address, cite, bdo, a
+  */
