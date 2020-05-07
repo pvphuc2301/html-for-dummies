c1c68dff1b075bf158e2d57f317ff48837d1f233
Phan Phuc <pvphuc2301@gmail.com>
2020-05-07 20:42:41 +0700

    modify exercise 1

--- html-structure-and-terms/1/index.js
+++ html-structure-and-terms/1/index.js
@@ -3,6 +3,6 @@
  * Giải thích theo ý hiểu của bạn ở dưới, không hiểu có thể hỏi trên Coders.Tokyo Slack hoặc JsLand 
  * Trả lời:
  * - khi load page, file html sễ được load từ trên xuống dưới
- * - files style đặt trong <head> sẽ được load trước, các định dạng style (element, id, class, ...) đã được định nghĩa khi load đến các thẻ elements sẽ hiện thị ra ngay
- * - files JavaScript đặt ở cuối của <body> để giảm khoảng thời gian phải load file javascript (do trong thời gian load file màn hình sẽ hiện trang trắng), ưu tiên hiển thị nội dung trước, file javascript có thể sẽ tương tác với các elements nên cần phải load các elements trước
+ * - files style đặt trong <head> sẽ được load trước, các định dạng style (element, id, class, ...) đã được định nghĩa khi load đến các thẻ elements sẽ hiện ra ngay
+ * - files JavaScript đặt ở cuối của <body> để  tránh khoảng thời gian phải load file javascript trước (do trong thời gian load file màn hình sẽ hiện trang trắng), ưu tiên hiển thị nội dung trước, file javascript có thể sẽ tương tác với các elements nên cần phải load các elements trước.
  */
