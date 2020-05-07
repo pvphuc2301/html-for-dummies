7b7d0986c85576817e767bf5861fab65c46bb91a
Phan Phuc <pvphuc2301@gmail.com>
2020-05-07 20:05:47 +0700

    adding element tags

--- html/1/index.js
+++ html/1/index.js
@@ -8,4 +8,5 @@
 
  /**
   * 2020-05-6 h1, h2, h3, h4, h5, h6, hr, html, head, title, meta, body, p, br, pre, b, strong, i, em, mark, small, del, ins, sup, sub, q, blockquote, abbr, address, cite, bdo, a
+  * 2020-05-7 img, map, area, picture, source, table, tr, th, td, ul, ol, li, dl, dt, dd, div, span, iframe, script, noscript, head, title, base, link, meta, style, code, kbd, samp, var, article, aside, details, figcaption, figure, footer, header, main, nav, section, summary, time
   */
