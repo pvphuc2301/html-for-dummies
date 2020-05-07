3de94c140655b8a2ab8d4df6f9834fb0f12c789b
Phan Phuc <pvphuc2301@gmail.com>
2020-05-07 20:56:20 +0700

    adding exercise 2

---
+++ html-structure-and-terms/2/index.js
@@ -0,0 +1,17 @@
+/**
+ * Tìm hiểu về thẻ meta, tìm các ví dụ thực tế dùng thẻ meta.
+ * Bạn có biết: Bạn có thể thay đổi hình ảnh preview khi chia sẻ đường link trang web của mình bằng cách thêm thẻ meta theo cấu trúc cung cấp bởi facebook.
+ * 
+ * Mở https://developers.facebook.com/tools/debug/
+ * Thử nhập coders.tokyo rồi ấn Debug
+ * Trả lời:
+ * - dùng để định nghĩa các từ khóa của trang sử dụng cho các công cụ tìm kiếm
+ *  <meta name="keywords" content="HTML, CSS, JavaScript">
+ * - mô tả trang web
+ * <meta name="description" content="Free Web tutorials for HTML and CSS">
+ * - xác định tác giả
+ * <meta name="author" content="John Doe">
+ * - hiển thị thích hợp vói các thiết bị có kích thước khác nhau
+ * <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ * 
+ */
