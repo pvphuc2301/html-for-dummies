5245b8d1f4e300124c8f34ad3c37b86b1427a5a8
Phan Phuc <pvphuc2301@gmail.com>
2020-05-08 20:10:46 +0700

    adding exercise 2

---
+++ document-object/2/index.js
@@ -0,0 +1,8 @@
+/**
+ * 1. Property nào của document lưu tham chiếu tới các <img> tags trong trang web
+ * 2. Sử dụng inspector để xem constructor function của một <img> có tên là gì
+ * 
+ * Trả lời:
+ * 1. document.images
+ * 2. HTMLImageElement()
+ *  */ 
